5f443157603461b27eccecca89e40fe4539ad7ad
SardorK <sardorkarshiev6167@gmail.com>
2022-08-09 04:52:45 +0500

    Initial Commit

---
+++ MovieAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPI
+//
+//  Created by User 2 on 09/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
